11c66aac5305c575624dd00e9c0664d894574086
Arjun Mehta <arjun@sighten.io>
2017-06-21 18:30:27 +0530

    Register facebook webhook

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+//server front page
+app.get('/', function(req, res){
+    res.send('This is the NBA Bot test');
+});
+
+//Facebook webhook
+app.get('/webhook', function(res, req) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
